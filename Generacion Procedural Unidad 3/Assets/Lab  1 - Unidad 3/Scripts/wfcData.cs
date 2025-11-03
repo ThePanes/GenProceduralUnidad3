e755d6f9421a7e71521343af7a0f7e45b4082936
dijin7 <73197968+dijin7@users.noreply.github.com>
2025-11-03 10:40:21 -0300

    Identificador

--- Generacion Procedural Unidad 3/Assets/Lab  1 - Unidad 3/Scripts/wfcData.cs
+++ Generacion Procedural Unidad 3/Assets/Lab  1 - Unidad 3/Scripts/wfcData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class wfcData : MonoBehaviour
 {
 
+    public int ID;
+
     public int xplus;
     public int zplus;
     public int xless;
---
+++ Generacion Procedural Unidad 3/Assets/Lab 2/wfc mod lec.cs
@@ -0,0 +1,139 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wfcmodlec : MonoBehaviour
+{
+    [Header("Prefabs con TileInfo")]
+    public GameObject[] prefabs; // cada prefab tiene un TileInfo
+    public int width = 10;
+    public int height = 10;
+
+    private List<GameObject>[,] possibilities;
+    private GameObject[,] placedTiles;
+
+    void Start()
+    {
+        // Carga el archivo como TextAsset
+        TextAsset txt = Resources.Load<TextAsset>("matriz");
+
+        // Divide por líneas
+        string[] lineas = txt.text.Split('\n');
+
+        // Determina tamaño
+        int filas = lineas.Length;
+        int columnas = lineas[0].Trim().Split(' ').Length;
+
+        // Crea la matriz
+        int[,] matriz = new int[filas, columnas];
+
+        for (int i = 0; i < filas; i++)
+        {
+            string[] valores = lineas[i].Trim().Split(' ');
+            for (int j = 0; j < columnas; j++)
+            {
+                matriz[i, j] = int.Parse(valores[j]);
+            }
+        }
+
+
+        possibilities = new List<GameObject>[width, height];
+        placedTiles = new GameObject[width, height];
+
+        // Inicializamos todas las celdas con todos los prefabs posibles
+        for (int x = 0; x < width; x++)
+            for (int z = 0; z < height; z++)
+                possibilities[x, z] = new List<GameObject>(prefabs);
+
+        // Empieza el proceso
+        StartCoroutine(CollapseRoutine());
+    }
+
+    System.Collections.IEnumerator CollapseRoutine()
+    {
+        while (true)
+        {
+            // Buscar la celda con menor entropía (menos opciones)
+            Vector2Int pos = GetCellWithLowestEntropy();
+            if (pos.x == -1) yield break; // terminado
+
+            // Escoger un prefab al azar
+            GameObject chosenPrefab = possibilities[pos.x, pos.y][Random.Range(0, possibilities[pos.x, pos.y].Count)];
+            wfcData chosenInfo = chosenPrefab.GetComponent<wfcData>();
+
+            // Instanciarlo en la escena
+            Vector3 position = new Vector3(pos.x, 0, pos.y);
+            placedTiles[pos.x, pos.y] = Instantiate(chosenPrefab, position, chosenPrefab.transform.rotation);
+
+
+            // Colapsar celda
+            possibilities[pos.x, pos.y] = new List<GameObject> { chosenPrefab };
+
+            // Propagar restricciones
+            Propagate(pos.x, pos.y, chosenInfo);
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    Vector2Int GetCellWithLowestEntropy()
+    {
+        int minCount = int.MaxValue;
+        Vector2Int pos = new Vector2Int(-1, -1);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                int count = possibilities[x, z].Count;
+                if (count > 1 && count < minCount)
+                {
+                    minCount = count;
+                    pos = new Vector2Int(x, z);
+                }
+            }
+        }
+        return pos;
+    }
+
+    void Propagate(int x, int z, wfcData tile)
+    {
+        CheckNeighbor(x + 1, z, tile, "xplus");
+        CheckNeighbor(x - 1, z, tile, "xless");
+        CheckNeighbor(x, z + 1, tile, "zplus");
+        CheckNeighbor(x, z - 1, tile, "zless");
+    }
+
+    void CheckNeighbor(int nx, int nz, wfcData origin, string dir)
+    {
+        if (nx < 0 || nz < 0 || nx >= width || nz >= height) return;
+
+        List<GameObject> valid = new List<GameObject>();
+
+        foreach (var candidate in possibilities[nx, nz])
+        {
+            wfcData info = candidate.GetComponent<wfcData>();
+            bool compatible = false;
+
+            switch (dir)
+            {
+                case "xplus": compatible = (origin.xplus == info.xless); break;
+                case "xless": compatible = (origin.xless == info.xplus); break;
+                case "zplus": compatible = (origin.zplus == info.zless); break;
+                case "zless": compatible = (origin.zless == info.zplus); break;
+            }
+
+            if (compatible) valid.Add(candidate);
+        }
+
+        if (valid.Count != possibilities[nx, nz].Count)
+        {
+            possibilities[nx, nz] = valid;
+            if (valid.Count == 1)
+            {
+                // Si el vecino colapsó a una sola opción, seguimos propagando
+                var chosenInfo = valid[0].GetComponent<wfcData>();
+                Propagate(nx, nz, chosenInfo);
+            }
+        }
+    }
+}
